8ec7aa5728ea3351c7b0aeb17cfc94adef928629
Francisco Campos <franciiscocampos170@gmail.com>
2020-06-22 12:10:05 +0100

    post migration created

--- database/migrations/2020_06_22_110804_posts.php
+++ database/migrations/2020_06_22_110804_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class Posts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::table('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
@@ -29,6 +31,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
