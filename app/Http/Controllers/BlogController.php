b4d237a656eb14021fd0cf9a781485246f047795
Francisco Campos <franciiscocampos170@gmail.com>
2020-06-22 11:50:30 +0100

    initial commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class BlogController extends Controller
+{
+    public function addRole()
+    {
+        $role = Role::create(['name' => 'collaborator']);
+    }
+    public function addPermission()
+    {
+        $permission = Permission::create(['name' => 'view-post']);
+    }
+
+    public function givePermissionToRole($id)
+    {
+        $role = Role::findById($id);
+        $permission = Permission::findById(1);
+        $role->givePermissionTo($permission);
+    }
+
+    public function giveRoleToUser()
+    {
+        $user = User::findOrFail(1);
+        $user->assignRole('collaborator');
+    }
+
+    public function getUserPermission()
+    {
+        $user = User::findOrFail(1);
+        return $permissionNames = $user->getRoleNames();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/addRole', 'BlogController@addRole')->name('add.role');
+Route::get('/addPermission', 'BlogController@addPermission')->name('add.permission');
+Route::get('/addPermissonToRole/{id}', 'BlogController@givePermissionToRole')->name('add.permission.to.role');
+Route::get('/addRoleToUser', 'BlogController@giveRoleToUser')->name('add.role.to.user');
+Route::get('/getUserPermission', 'BlogController@getUserPermission')->name('get.user.permission');
+
